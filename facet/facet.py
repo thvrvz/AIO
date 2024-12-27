8af122beece22c56e738a754c14db602563085d5
thvrvz <ariiwpdcod@gmail.com>
2024-12-27 13:35:37 +0330

    push

--- facet/facet.py
+++ facet/facet.py
@@ -1,2 +1,18 @@
-import matplotlib , cv2
+import matplotlib
 from matplotlib import pyplot as plt
+import cv2
+
+image = cv2.imread("facet/Albert.jpg")
+image = cv2.cvtColor(image ,cv2.COLOR_BGR2RGB)
+
+plt.imshow(image)
+model = cv2.CascadeClassifier("facet/model.xml")
+facet = model.detectMultiScale(image)
+
+a = facet[0][0]
+b = facet[0][1]
+c = facet[0][2]
+d = facet[0][3]
+
+image = cv2.rectangle( image , (a,b) , (a+c,b+d) , (0,255,255) , 3 )
+
